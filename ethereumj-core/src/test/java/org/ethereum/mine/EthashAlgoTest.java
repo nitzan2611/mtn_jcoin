43602cc6d5c001590fb013941a7a8e6f0cd8df29
nitzan farhi <nitzan2611@gmail.com>
2018-02-01 14:13:42 +0200

    Ahalhn

---
+++ ethereumj-core/src/test/java/org/ethereum/mine/EthashAlgoTest.java
@@ -0,0 +1,46 @@
+package org.ethereum.mine;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by admin on 14/01/2018.
+ */
+public class EthashAlgoTest {
+    @Test
+    public void hashimoto() throws Exception {
+
+    }
+
+    @Test
+    public void hashimotoLight() throws Exception {
+
+    }
+
+    @Test
+    public void hashimotoFull() throws Exception {
+
+    }
+
+    @Test
+    public void mine() throws Exception {
+
+    }
+
+    @Test
+    public void mine1() throws Exception {
+
+    }
+
+    @Test
+    public void mineLight() throws Exception {
+
+    }
+
+    @Test
+    public void mineLight1() throws Exception {
+
+    }
+
+}
